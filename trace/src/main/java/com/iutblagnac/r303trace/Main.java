b57163bd4bed4130cf29876efbf44b4f3cf6019c
AlexVignal <95383900+AlexVignal@users.noreply.github.com>
2023-09-25 00:45:05 +0200

    Add files via upload

---
+++ trace/src/main/java/com/iutblagnac/r303trace/Main.java
@@ -0,0 +1,20 @@
+package org.example;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println(hello());
+    }
+
+    public static String hello() {
+        return  "Hello World!" ;
+    }
+
+    public static String hello(String param) {
+        return  param ;
+    }
+
+}
